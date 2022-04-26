88f07f7b57d43fc29e50734630855b2021d2cfed
isaicastro1 <63988161+isaicastro1@users.noreply.github.com>
2022-04-25 21:58:06 -0700

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    },
+    {
+        id: 2,
+        name: 'Ale Ramos',
+        email: 'aramos@gmail.com'
+
+    },
+    {
+        id: 3,
+        name: 'Sammy Pelayo',
+        email: 'sammys@gmail.com'
+
+    },
+    {
+        id: 4,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    },
+    {
+        id: 5,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    },
+    {
+        id: 6,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    },
+    {
+        id: 7,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    },
+    {
+        id: 8,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    },
+    {
+        id: 9,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    },
+    {
+        id: 10,
+        name: 'Isai Castro',
+        email: 'isaicastro@gmail.com'
+
+    }
+];
