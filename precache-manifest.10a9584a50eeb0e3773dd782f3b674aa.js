7bfc1a1cffdf6048ba73e8c0b45660ed4669ea05
isaicastro1 <isaicstr@gmail.com>
2022-05-10 20:59:53 -0700

    Updates

---
+++ precache-manifest.10a9584a50eeb0e3773dd782f3b674aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d74168f7d63ceac44320",
+    "url": "/robofriends/static/css/main.deaa78d4.chunk.css"
+  },
+  {
+    "revision": "d74168f7d63ceac44320",
+    "url": "/robofriends/static/js/main.3a1a73f2.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "117fac2e999fb4fd6da5",
+    "url": "/robofriends/static/css/2.92498b61.chunk.css"
+  },
+  {
+    "revision": "117fac2e999fb4fd6da5",
+    "url": "/robofriends/static/js/2.9034e1f4.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "030724a8ad82754d40477f570b90c648",
+    "url": "/robofriends/index.html"
+  }
+];
