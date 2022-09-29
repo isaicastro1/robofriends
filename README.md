a68062c1eac9eb6fe9a8d9ed9ab410193c3de84f
Isai Castro <isaicstr@gmail.com>
2022-09-28 21:21:21 -0700

    Finished implementing hooks and adjusting indentation in all files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Robofriends Project
 
-Contains list of robots and a search bar that allows you to search the robots by name and automatically reloads with the robots containing what you are typing in the search box. Made with React.
+Contains list of robots and a search bar that allows you to search the robots by name and automatically reloads with the robots containing what you are typing in the search box.
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ErrorBoundary extends Component {
-    constructor() {
-        super();
-        this.state = {
-            hasError: false,
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      hasError: false,
+    };
+  }
 
-    componentDidCatch() {
-        this.setState({hasError: true})
-    }
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
 
-    render() {
-        if (this.state.hasError === true) {
-            return <h1>Ooops. Something went wrong</h1>
-        }
-        else {
-            return (
-                this.props.children
-            )
-        }
+  render() {
+    if (this.state.hasError === true) {
+      return <h1>Ooops. Something went wrong</h1>;
+    } else {
+      return this.props.children;
     }
+  }
 }
 
 export default ErrorBoundary;
